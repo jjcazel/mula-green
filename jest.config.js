6250e07aaed5496bcb647be4aaf2daf5d2c1e889
Jared Cazel <jjcazel@gmail.com>
2024-03-12 17:06:45 -0400

    Remove babel config files and put the jest babel config rules in the jest config file.

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,18 @@
+// jest.config.js
 module.exports = {
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  transform: {
+    // Transform files with a '.js' or '.jsx' extension using 'babel-jest'
+    "^.+\\.(js|jsx|ts|tsx)$": [
+      "babel-jest",
+      {
+        presets: [
+          ["@babel/preset-env", { targets: { node: "current" } }],
+          "@babel/preset-react",
+          "@babel/preset-typescript",
+        ],
+      },
+    ],
+  },
 };
